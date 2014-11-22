3635b55deadbf8399910a5d97832cb3709566791
bergurth <bergurthssn@gmail.com>
2014-11-22 01:40:31 +0000

    fusion from thor game, almost compleat, everything except troll ouch, and sound and sceanery change

---
+++ gameC/js/code2.js
@@ -0,0 +1,337 @@
+		
+
+
+var layer1;
+var ctx1;
+var canvaswidth;
+var canvasheight;
+
+
+var gameLoopCounter = 0;
+var chardirection = "down";
+var charMoving = false;
+var direction_arr = {"down": 0, "left": 1, "right": 2, "up": 3};
+
+
+var sprite_offset_x = 46;
+var sprite_offset_y = 20;
+
+
+
+$(document).ready(function() {
+
+   
+	music.play();
+    layer1 = document.getElementById('layer1'); // ná canvas!
+	var t=setInterval("gameLoop()",refreshRate);
+	
+	    $('.canvas-container').mousedown(function myDown(e) {
+        countAmmunition--;
+		shot.playclip();
+        var position = $(layer1).offset(); // hér var $(example).position() sem var ekki lengur rétt
+        var x = e.pageX-position.left;
+        var y = e.pageY-position.top;
+        //drawGunshot(x,y);
+        drawLaser(x,y);
+        // Hitti fuglinn eða ekki .. 
+        // Þetta fyrir neðan notar kassa, þurfum pythagorean distance fyrir hringinn
+        if( (birdxpos-birdsize/2)<x && x<(birdxpos+birdsize/2) 
+        && (birdypos-birdsize/2)<y && y<(birdypos+birdsize/2) 
+        && countAmmunition > 0) {
+            // some winning condition
+			wilhelm_scream.playclip();
+            countShotBirds++;
+            createRandomBird();
+        }
+    });
+
+  
+
+   // layer1.width = $('.canvas-container').width();
+
+    layer1.width = 900;   
+    layer1.height = 900;
+    
+    canvaswidth = layer1.width;
+    canvasheight = layer1.height;
+    ctx1 = layer1.getContext('2d');
+
+    //other layers
+    layer2 = document.getElementById("layer2");
+    layer2.width = 900;
+    layer2.height = 900;
+    ctx2 = layer2.getContext("2d");
+    layer3 = document.getElementById("layer3");
+    layer3.width = 900;
+    layer3.height = 900;
+    ctx3 = layer3.getContext("2d");
+    layer4 = document.getElementById("layer4");
+    layer4.width = 900;
+    layer4.height = 900;
+    ctx4 = layer4.getContext("2d");
+    
+    TIEwar.onload = function() {ctx1.drawImage(TIEwar,0,0,canvaswidth,canvasheight);};
+
+	
+	initiateGame(layer1,layer2,layer3,layer4);
+    
+	
+});
+
+
+
+
+function initiateGame(layer1,Layer2,Layer3,Layer4) {
+
+
+
+
+fps = 50;
+ctx1;
+ctx2;
+ctx3;
+ctx4;
+canvaswidth;
+ canvasheight;
+ birdxpos = 100;
+ birdypos = 100;
+ birdsize = 20;
+ birdcolor = "rgb(255,0,0)";
+ updown = "up";
+ leftright="left";
+ refreshRate = 1000/fps;
+ speed = 100/fps;
+ countShotBirds = 0;
+ countAmmunition = 100;
+
+
+    layer1.width = 900;
+    layer1.height = 900;
+
+    
+    
+    canvaswidth = layer1.width;
+    canvasheight = layer1.height;
+    ctx1 = layer1.getContext('2d');
+
+    //other layers
+    layer2.width = 900;
+    layer2.height = 900;
+    ctx2 = layer2.getContext("2d");
+    layer3.width = 900;
+    layer3.height = 900;
+    ctx3 = layer3.getContext("2d");
+    layer4.width = 900;
+    layer4.height = 900;
+    ctx4 = layer4.getContext("2d");
+
+    
+    ctx1.drawImage(TIEwar,0,0,canvaswidth,canvasheight);
+    
+}
+
+function clearCharacter() {
+    ctx3.clearRect(xpos,ypos,width,height);
+}
+
+function moveCharacter() {
+    if(keydown.left || keydown.a) {
+        xpos -= 2;
+        chardirection = "left";
+    }   
+    if(keydown.right || keydown.d) {
+        xpos += 2;
+        chardirection = "right";
+    }
+    if(keydown.down || keydown.s) {
+        ypos += 2;
+        chardirection = "down";
+    }
+    if(keydown.up || keydown.w) {
+        ypos -= 2;
+        chardirection = "up";
+    }
+    if (keydown.left || keydown.right || keydown.down || keydown.up || keydown.a || keydown.s || keydown.d || keydown.w) {
+        charMoving = true;
+    }
+    else charMoving = false;
+}
+
+function drawCharacter() {
+    if(charMoving) {
+        gameLoopCounter++;
+        if(gameLoopCounter == 10) {
+            if (sx >= 3*swidth) {
+                sx = xstart;
+            }
+            else {
+                sx += swidth;
+            }
+            gameLoopCounter = 0;
+        }
+    }
+    
+    sy = direction_arr[chardirection] * sheight;
+    ctx3.drawImage(imgChar, sx, sy, swidth, sheight, xpos, ypos, width, height);
+}
+
+function createRandomBird() {
+			var type = Math.random()<0.5;
+			if (type){
+				birdKind = "horiz";
+			}
+			else {
+				birdKind = "vertical";
+			}
+			var random = Math.floor(Math.random()*canvaswidth);
+            birdxpos=random;
+            random = Math.floor(Math.random()*canvasheight);
+            birdypos=random;
+            var random1 = Math.floor(Math.random()*150)+50; 
+            var random2 = Math.floor(Math.random()*150)+50; 
+            var random3 = Math.floor(Math.random()*150)+50; 
+            birdcolor = "rgb("+random1+","+random2+","+random3+")";
+}
+
+function writeGameState() {    
+    ctx3.fillStyle = '#000';
+    ctx3.font = '30px verdana';
+    ctx3.textBaseline = 'top';
+    ctx3.fillText('You have '+countAmmunition+' shots left',30,0);
+    ctx3.fillText('You have shot '+countShotBirds+' balloons.',30,30);
+}
+
+function writeGameOver() {    
+    ctx3.fillStyle = '#000';
+    ctx3.font = '50px verdana';
+    ctx3.textBaseline = 'top';
+    ctx3.fillText('Game over! You got '+countShotBirds+' balloons',30,0);
+    ctx3.font = '30px verdana';
+    ctx3.fillText('To play again, hit New Game!',30,50);
+    exit();
+}
+
+function clearGameState() {
+    ctx3.fillStyle = "rgb(255,255,255)";
+    ctx3.fillRect(0,0,500,100);  
+}
+
+function gameLoop() {
+    // draw entire background??
+    clearGameState();
+    if (countAmmunition > 0) writeGameState();
+    else writeGameOver();
+    clearBird();
+    moveBird();
+    collisionWithWalls();
+    drawBird();
+
+    clearCharacter();
+    moveCharacter();
+    drawCharacter();
+}
+
+function drawGunshot(x, y) {
+    ctx1.fillStyle = "rgb(80,80,80)";
+    ctx1.beginPath();
+    ctx1.arc(x, y, 4, 12, Math.PI*2, true); 
+    ctx1.closePath();
+    ctx1.fill();  
+    ctx1.fillStyle = "rgb(0,0,0)";
+    ctx1.beginPath();
+    ctx1.arc(x, y, 4, 9, Math.PI*2, true); 
+    ctx1.closePath();
+    ctx1.fill();        
+}
+function drawLaser(x, y) {
+    // xpos = char x pos
+    ctx4.strokeStyle = "rgb(255,0,0)";
+    ctx4.lineWidth=2;
+    ctx4.beginPath();
+    switch(chardirection) {
+        case "down":
+            ctx4.moveTo(xpos+sprite_offset_x-4, ypos+sprite_offset_y);
+            break;
+        case "up":
+            ctx4.moveTo(xpos+sprite_offset_x-10, ypos+sprite_offset_y -20);
+            break;
+        case "left":
+            ctx4.moveTo(xpos+sprite_offset_x-20, ypos+sprite_offset_y +5);
+            break;
+        case "right":
+            ctx4.moveTo(xpos+sprite_offset_x+20, ypos+sprite_offset_y +5);
+            break;
+    }
+    ctx4.lineTo(x, y);
+    ctx4.stroke();
+    ctx4.beginPath();
+    switch(chardirection) {
+        case "down":
+            ctx4.moveTo(xpos+sprite_offset_x+4, ypos+sprite_offset_y);
+            break;
+        case "up":
+            ctx4.moveTo(xpos+sprite_offset_x+4, ypos+sprite_offset_y -20);
+            break;
+        case "left":
+            ctx4.moveTo(xpos+sprite_offset_x-26, ypos+sprite_offset_y);
+            break;
+        case "right":
+            ctx4.moveTo(xpos+sprite_offset_x+26, ypos+sprite_offset_y);
+            break;
+    }
+    ctx4.lineTo(x, y);
+    ctx4.stroke();
+    // setTimeout( clearLaser(xpos+sprite_offset_x, ypos+sprite_offset_y, x, y), 10000);
+    setTimeout(function() {ctx4.clearRect(0, 0, 1000, 1000)}, 50);
+}
+
+function drawBird() {
+    ctx2.fillStyle = birdcolor;
+    ctx2.beginPath();
+    var radius = birdsize/2;
+    ctx2.arc(birdxpos, birdypos, radius, 13, Math.PI*2, true); 
+    ctx2.closePath();
+    ctx2.fill();
+    ctx2.drawImage(TIE1,birdxpos-radius*0.8,birdypos-radius*0.8);
+}
+
+function clearBird() {
+    ctx2.fillStyle = "rgb(255,255,255)";
+    ctx2.beginPath();
+    var radius = birdsize/2;
+    ctx2.arc(birdxpos, birdypos, radius, 13, Math.PI*2, true); 
+    ctx2.closePath();
+    ctx2.fill(); 
+}
+
+function moveBird() {
+	var random =Math.floor(Math.random()*45);
+	if(birdKind=="horiz"){
+    
+    if(random == 5) updown = "up";
+    if(random == 6) updown = "down";
+    if(updown =="up") birdypos -= speed;
+    else birdypos += speed;
+
+    if(leftright =="right") birdxpos += speed;
+    else birdxpos -= speed;
+	}
+	else {
+   
+    if(random == 5) leftright = "up";
+    if(random == 6) leftright = "down";
+    if(updown =="up") birdypos -= speed;
+    else birdypos += speed;
+
+    if(leftright =="right") birdxpos += speed;
+    else birdxpos -= speed;
+	}
+}
+
+function collisionWithWalls() {
+    if(birdxpos < birdsize/2) leftright="right";
+    if(birdxpos > (canvaswidth-birdsize/2) ) leftright="left";
+    
+    if(birdypos < birdsize/2) updown="down";
+    if(birdypos > (canvasheight-birdsize/2)) updown="up";
+}
---
+++ gameC/js/config.js
@@ -0,0 +1,94 @@
+
+// general settings
+var fps = 50;
+var refreshRate = 1000/fps;
+var speed = 100/fps;
+var countShotBirds = 0;
+var countAmmunition = 100;
+
+//initial bird situation
+var birdxpos = 100;
+var birdypos = 100;
+var birdsize = 20;
+var birdcolor = "rgb(255,0,0)";
+var updown = "up";
+var leftright="left";
+birdKind = "horiz";
+
+//audio
+var html5_audiotypes={
+	"mp3": "audio/mpeg",
+	"mp4": "audio/mp4",
+	"ogg": "audio/ogg",
+	"wav": "audio/wav"
+}
+
+function createsoundbite(sound){
+	var html5audio=document.createElement('audio');
+	$('#player').append(html5audio);
+  	html5audio.id = "html5audio";
+	if (html5audio.canPlayType){
+		for (var i=0; i<arguments.length; i++){
+			var sourceel=document.createElement('source')
+			sourceel.setAttribute('src', arguments[i])
+			if (arguments[i].match(/\.(\w+)$/i))
+				sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
+			html5audio.appendChild(sourceel)
+		}
+		html5audio.load()
+		html5audio.playclip=function(){
+			html5audio.pause()
+			html5audio.currentTime=0
+			html5audio.play()
+		}
+		return html5audio
+	}
+	else{
+		return {playclip:function(){throw new Error("Sorry brah, yo browser too ol' get Chrome or sumtin!")}}
+	}
+}
+
+
+var music = new Audio('../sounds/bergur_music.mp3');
+music.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+
+var shot=createsoundbite("../sounds/sniper_shot.wav");
+	shot.volume=0.05;
+var wilhelm_scream=createsoundbite('../sounds/wilhelm_scream.mp3');
+	wilhelm_scream.volume = 0.25;
+
+
+// Images
+
+var TIEwar = new Image();
+TIEwar.src ='../images/tie-fighter-space-wars.jpg'; 
+
+var TIE1 = new Image();
+//TIE1.src ='../images/TIE2.jpg';
+TIE1.src = "../images/280px-PNG_transparency_demonstration_1.png";
+
+//var troll = new Image();
+//troll.src ='../images/trollhammer.png';
+
+// troll position
+var imgChar = new Image();
+//imgChar.src = "http://www.elfquest.com/social/file/pic/photo/2012/01/TrollHammer-ogre_500.png";
+imgChar.src = "../images/trollhammer.png";
+var xpos = 100;
+var ypos = 100;
+var width = 91;
+var height = 125;
+
+var xstart = 4
+var sx = xstart;
+var sy = 0;
+var swidth = width;
+var sheight = height;
+
+var gameLoopCounter = 0;
+var chardirection = "down";
+var charMoving = false;
+var direction_arr = {"down": 0, "left": 1, "right": 2, "up": 3};
